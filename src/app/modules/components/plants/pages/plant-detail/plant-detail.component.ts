4f987e243715f2cab69bb89a8c9adddd4c2f7d4b
beytuuh42 <beytuh94@web.de>
2024-04-26 01:52:12 +0200

    enhanced plant detail page: added carousel to show plants, using async observables for templating

--- src/app/modules/components/plants/pages/plant-detail/plant-detail.component.ts
+++ src/app/modules/components/plants/pages/plant-detail/plant-detail.component.ts
@@ -1,24 +1,32 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
-import { IPlant } from '@app/core/models/plant.model';
-import { PlantService } from '@app/core/services/plants/plant.service';
+import { Observable, switchMap } from 'rxjs';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { IPlant } from '@app/core/models/plant.model';
+import { IPlantImage } from '@app/core/models/plant-image.model';
+import { PlantService } from '@app/core/services/plants/plant.service';
 @Component({
   selector: 'app-plant-detail',
   standalone: true,
-  imports: [],
+  imports: [NgbCarouselModule, AsyncPipe],
   templateUrl: './plant-detail.component.html',
   styleUrl: './plant-detail.component.scss',
 })
-export class PlantDetailComponent {
-  private plantService = inject(PlantService);
-  route = inject(ActivatedRoute);
-  plant: IPlant | undefined;
+export class PlantDetailComponent implements OnInit {
+  private route = inject(ActivatedRoute);
+  protected plantService = inject(PlantService);
 
-  constructor() {
+  plant$!: Observable<IPlant>;
+  plantImages$!: Observable<IPlantImage[]>;
+
+  ngOnInit(): void {
     const plantId = Number(this.route.snapshot.paramMap.get('id'));
-    this.plantService.get(plantId).subscribe((data) => {
-      this.plant = data;
-    });
+
+    this.plant$ = this.plantService.get(plantId);
+    this.plantImages$ = this.plant$.pipe(
+      switchMap((plant) => this.plantService.getImagesList(plant.id))
+    );
   }
 }
