6f775366a26c167799f6d3a2ffd8a0ec27e7b375
beytuuh42 <beytuh94@web.de>
2024-04-26 01:52:58 +0200

    enhanced plant list page: using async observables for plants; minor styling

--- src/app/modules/components/plants/pages/plant-list/plant-list.component.ts
+++ src/app/modules/components/plants/pages/plant-list/plant-list.component.ts
@@ -2,45 +2,33 @@ import { Component, inject, OnInit, TemplateRef } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { IPlant } from '@app/core/models/plant.model';
 import { PlantService } from '@app/core/services/plants/plant.service';
-import { tap } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { JsonPipe } from '@angular/common';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-plant-list',
   standalone: true,
-  imports: [RouterLink, JsonPipe],
+  imports: [RouterLink, JsonPipe, AsyncPipe],
   templateUrl: './plant-list.component.html',
   styleUrl: './plant-list.component.scss',
 })
 export class PlantListComponent {
   private modalService = inject(NgbModal);
+  protected plantService = inject(PlantService);
 
-  plantService = inject(PlantService);
-  plants: IPlant[] = [];
+
+  plants$!: Observable<IPlant[]>;
 
   ngOnInit(): void {
-    this.plantService
-      .getList()
-      .pipe(
-        tap((data) => {
-          console.log(data);
-        })
-      )
-      .subscribe((data) => {
-        this.plants = data;
-      });
-
-    this.plantService.getList().subscribe((data) => {
-      this.plants = data;
-    });
+    this.plants$ = this.plantService.getList();
   }
 
   onEdit(content: TemplateRef<any>, plant: IPlant) {
-    const modalRef = this.modalService.open(content, { size: 'lg' });
-    console.log(plant);
-    console.log('modalRef.componentInstance:', modalRef.componentInstance);
-
-    modalRef.componentInstance.plant = plant;
+    // const modalRef = this.modalService.open(content, { size: 'lg' });
+    // console.log(plant);
+    // console.log('modalRef.componentInstance:', modalRef.componentInstance);
+    // modalRef.componentInstance.plant = plant;
   }
 }
