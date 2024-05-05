368803afea93e06cc74fab406f7a89016244a4ff
beytuuh42 <46460629+beytuuh42@users.noreply.github.com>
2024-05-05 21:41:16 +0000

    Deploying to gh-pages from @ beytuuh42/planteria-fe-angular@11d96cc7a337bd49a02fd08053e79f52ee3a56a0 🚀

---
+++ browser/chunk-QB7LCW4O.js
@@ -0,0 +1,1 @@
+import{bb as o,cb as s,ea as i,rb as a,vb as r}from"./chunk-XKGJ2RL6.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=i({type:e,selectors:[["app-settings"]],standalone:!0,features:[r],decls:2,vars:0,template:function(n,c){n&1&&(o(0,"p"),a(1,"settings works!"),s())}});let t=e;return t})();export{d as SettingsComponent};
